9a29e5a018780e9cc49d3ec1c002dd4f140a3a47
Alberto Migliorato <dev@albemiglio.it>
2020-07-25 18:14:43 +0200

    First commit

---
+++ src/main/java/it.albemiglio.chancelib/ChanceLib.java
@@ -0,0 +1,14 @@
+package it.albemiglio.chancelib;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChanceLib extends JavaPlugin {
+
+    @Getter
+    ChanceLib instance;
+
+    public void onEnable() {
+        instance = this;
+    }
+}
