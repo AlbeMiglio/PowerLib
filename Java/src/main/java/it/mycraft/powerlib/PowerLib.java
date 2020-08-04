8e8b0603188b7b678911293063b95a6c8152f0d8
FranFrau <frau-francesco@outlook.it>
2020-08-04 15:43:32 +0200

    v1.0.7 Update - Updated PowerLib.java class
    Signed-off-by: FranFrau <frau-francesco@outlook.it>

--- Java/src/main/java/it/mycraft/powerlib/PowerLib.java
+++ Java/src/main/java/it/mycraft/powerlib/PowerLib.java
@@ -1,15 +1,7 @@
 package it.mycraft.powerlib;
 
-import it.mycraft.powerlib.inventory.InventoryBuilder;
-import it.mycraft.powerlib.item.ItemBuilder;
 import lombok.Getter;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Arrays;
 
 /**
  * PowerLib Official Source code
@@ -24,46 +16,5 @@ public class PowerLib extends JavaPlugin {
 
     public void onEnable() {
         instance = this;
-
-        ItemStack item = new ItemBuilder()
-                .setMaterial("DIRT")
-                .setAmount(1)
-                .setName("Dirt")
-                .setLore(Arrays.asList("This is", "a dirt block"))
-                .setGlowing(true)
-                .build();
-
-        Inventory inventory = new InventoryBuilder()
-                .setRows(5)
-                .setTitle("&cThis is a title")
-                .fillInventory(new ItemBuilder()
-                        .setMaterial(Material.STAINED_GLASS_PANE)
-                        .setDamage((short) 15)
-                        .build())
-                .fillColumn(5, item)
-                .fillRow(3, new ItemStack(Material.STONE))
-                .setItem(22, new ItemBuilder()
-                        .setMaterial("DIAMOND_BLOCK")
-                        .setAmount(64)
-                        .setName("&f&k&lOO&r&6I'm the center&f&k&lOO")
-                        .setLore("&9This", "&9is", "&9an", "&9item")
-                        .setGlowing(true)
-                        .build())
-                .setItem(21, new ItemBuilder()
-                        .setMaterial(Material.QUARTZ_BLOCK)
-                        .build())
-                .setItem(23, new ItemBuilder()
-                        .setMaterial(Material.QUARTZ_BLOCK)
-                        .build())
-                .build();
-
-        new InventoryBuilder()
-                .setRows(1)
-                .setTitle("&8Title")
-                .fillInventory(new ItemBuilder()
-                        .setMaterial(Material.STAINED_GLASS_PANE)
-                        .setDamage((short) 7)
-                        .build())
-                .open(Bukkit.getPlayer("Player"));
     }
 }
